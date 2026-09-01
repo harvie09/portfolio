91da9c8b305bd6a922010f1aa9b8a7a76b1afb57
harvie09 <160248718+harvie09@users.noreply.github.com>
2026-09-01 19:35:53 +0800

    Add files via upload

---
+++ assets/js/particle.js
@@ -0,0 +1,14 @@
+particlesJS("particles-bg", {
+    particles: {
+        number: { value: 80 },
+        size: { value: 3 },
+        move: { speed: 2 },
+        line_linked: { enable: true, opacity: 0.2 },
+        opacity: { value: 0.6 },
+    },
+    interactivity: {
+        events: {
+            onhover: { enable: true, mode: "repulse" },
+        },
+    },
+});
